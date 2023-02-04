fcbfdf87ed4a2839b25c67b4892c28ad13ec61f4
vannd1109 <duynguyen1109gl@gmail.com>
2023-02-04 16:33:36 +0700

    Design PayRoll Screen

--- src/screens/PayRoll/index.jsx
+++ src/screens/PayRoll/index.jsx
@@ -1,20 +1,65 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
+import {useRef, useState} from 'react';
 
 const PayRoll = () => {
   const [selectedLanguage, setSelectedLanguage] = useState();
+
+  const pickerRef = useRef();
+
+  function open() {
+    pickerRef.current.focus();
+  }
+
+  function close() {
+    pickerRef.current.blur();
+  }
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Picker
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)
-        }>
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
-      </Picker>
+    <View>
+      <View style={styles.payrollOption}>
+        <View style={styles.payrollLabel}>
+          <Text>Chọn tháng</Text>
+        </View>
+        <View style={styles.payrollMoth}>
+          <Picker
+            ref={pickerRef}
+            selectedValue={selectedLanguage}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedLanguage(itemValue)
+            }>
+            <Picker.Item label="Tháng 1" value="java" />
+            <Picker.Item label="Tháng 2" value="js" />
+          </Picker>
+        </View>
+      </View>
+      <View style={styles.payrollContent}>
+        <Text>Bảng lương</Text>
+      </View>
     </View>
   );
 };
 
 export default PayRoll;
+
+const styles = StyleSheet.create({
+  payrollOption: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    borderBottomWidth: 1
+  },
+  payrollLabel: {
+    display: 'flex',
+    flex: 1,
+  },
+  payrollMoth: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    flex: 2
+  },
+  payrollContent: {
+    padding: 5
+  }
+});
